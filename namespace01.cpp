d8ec31e0ecfa3f75b813f0e572fa559386800716
Rishabharaj Sharma <137804352+rishabharaj@users.noreply.github.com>
2025-05-31 15:05:19 +0530

    Create namespace01.cpp

---
+++ namespace01.cpp
@@ -0,0 +1,18 @@
+// The following example shows how the scope resolution operator is used with namespaces and classes:
+namespace NamespaceA{
+    int x;
+    class ClassA {
+    public:
+        int x;
+    };
+}
+
+int main() {
+
+    // A namespace name used to disambiguate
+    NamespaceA::x = 1;
+
+    // A class name used to disambiguate
+    NamespaceA::ClassA a1;
+    a1.x = 2;
+}
