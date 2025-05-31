649dc348b0bb93a756fa719dee836bb0711ae890
Rishabharaj Sharma <137804352+rishabharaj@users.noreply.github.com>
2025-05-31 15:15:01 +0530

    Create namespace05.cpp

---
+++ namespace05.cpp
@@ -0,0 +1,16 @@
+// Use :: for static members
+// You must use the scope resolution operator to call static members of classes.
+
+class ClassG {
+public:
+    static int get_x() { return x;}
+    static int x;
+};
+
+int ClassG::x = 6;
+
+int main() {
+
+    int gx1 = ClassG::x;
+    int gx2 = ClassG::get_x();
+}
