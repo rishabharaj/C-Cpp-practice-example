4d804bda00bfa2a4161e47a7ad4d8d9e8ed77a67
Rishabharaj Sharma <137804352+rishabharaj@users.noreply.github.com>
2025-05-31 15:06:43 +0530

    Create namespace02.cpp

---
+++ namespace02.cpp
@@ -0,0 +1,18 @@
+// A scope resolution operator without a scope qualifier refers to the global namespace.
+namespace NamespaceA{
+    int x;
+}
+
+int x;
+
+int main() {
+    int x;
+
+    // the x in main()
+    x = 0;
+    // The x in the global namespace
+    ::x = 1;
+
+    // The x in the A namespace
+    NamespaceA::x = 2;
+}
