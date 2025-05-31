cadea388198358db5c975320d0ae7ff422ac365d
Rishabharaj Sharma <137804352+rishabharaj@users.noreply.github.com>
2025-05-31 15:17:11 +0530

    Update namespace05.cpp

--- namespace05.cpp
+++ namespace05.cpp
@@ -1,6 +1,7 @@
 // Use :: for static members
 // You must use the scope resolution operator to call static members of classes.
 
+#include<iostream>
 class ClassG {
 public:
     static int get_x() { return x;}
@@ -13,4 +14,5 @@ int main() {
 
     int gx1 = ClassG::x;
     int gx2 = ClassG::get_x();
+    std::cout << "gx1: " << gx1 << ", gx2: " << gx2 << std::endl;
 }
